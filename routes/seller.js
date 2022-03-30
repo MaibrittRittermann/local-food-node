1a616f7cfeacc1af3d32c3c48a2cc64d8d84f226
Maibritt <mri@skivecollege.dk>
2022-03-30 21:24:41 +0200

    Seller updated

--- routes/seller.js
+++ routes/seller.js
@@ -7,5 +7,53 @@ router.get('/', async(req, res) => {
     res.send(await Seller.find().sort("name"));
 });
 
-
+route.get("/:id", validateObjectID, async (req, res) => {
+    let seller = await Seller.findById(req.params.id);
+    if (!seller) return res.status(404).send("This seller does not exist!");
+    res.send(seller);
+  });
+  
+  route.get("/products/:id", validateObjectID, async (req, res) => {
+    let seller = await Seller.findById(req.params.id);
+    if (!seller) return res.status(404).send("This seller does not exist!");
+    res.send(seller.products);
+  });
+  
+  route.post("/", auth, async (req, res) => {
+    const { error } = validate(req.body);
+  
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const seller = new Seller({ ...req.body });
+    await seller.save();
+    res.send(seller);
+  });
+  
+  route.put("/:id", [auth, validateObjectID], async (req, res) => {
+    const { error } = validate(req.body);
+  
+    if (error) return res.status(400).send(error.details[0].message);
+    const seller = await Seller.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        address: req.body.address,
+        zip: req.body.zip,
+        city: req.body.city,
+        products: req.body.products,
+      },
+      { new: true }
+    );
+    res.send(seller);
+  });
+  
+  route.delete("/:id", [auth, validateObjectID], async (req, res) => {
+    let seller = Seller.findById(req.params.id);
+    if (!seller) return res.status(404).send("This seller does not exist!");
+  
+    await Seller.deleteOne({ _id: prod._id });
+  
+    res.send(seller);
+  });
+  
 module.exports = router;
