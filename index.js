e004da0e15a46b2603e56209719838ee76fa5cfb
Maibritt Rittermann <mri@skivecollege.dk>
2022-04-25 11:05:25 +0200

    Test logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.log("Test logging in heroku logs with console.log");
   res.send("Hello World");
 });
 
